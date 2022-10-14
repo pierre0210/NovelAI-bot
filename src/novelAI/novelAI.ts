d8d459b5b99f30f02be121d66daa19b441dfd47c
pierre0210 <gamelauncher0210@gmail.com>
2022-10-14 14:32:06 +0800

    feat: novelAI.ts done image download

--- src/novelAI/novelAI.ts
+++ src/novelAI/novelAI.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import fs from "node:fs";
+import path from "path";
+require("dotenv").config();
 
 class novelAI {
 	url: string;
@@ -7,17 +10,17 @@ class novelAI {
 	samplers: string[];
 
 	input: string;
-	numberOfPics: number;
 	model: string;
 	resolution: string;
 	scale: number;
 	sampler: string;
 	steps: number;
+	n_samples: number;
 	ucPreset: number;
 	uc: string;
 
-	constructor(input: string, numberOfPics: number, model: string, resolution: string, scale: number, sampler: string, steps: number, ucPreset: number, uc: string) {
-		this.url = "api.novelai.net";
+	constructor(input: string, n_samples: number, model: string, resolution: string, scale: number, sampler: string, steps: number, uc: string, ucPreset: number = 0) {
+		this.url = "https://api.novelai.net";
 		
 		this.resolutions = ["Portrait (Normal): 512x768",
 			"Landscape (Normal): 768x512",
@@ -34,17 +37,17 @@ class novelAI {
 		this.samplers = ['k_euler_ancestral', 'k_euler', 'k_lms', 'plms', 'ddim'];
 
 		this.input = input;
-		this.numberOfPics = numberOfPics;
 		this.model = this.models[model];
 		this.resolution = resolution;
 		this.scale = scale;
 		this.sampler = sampler;
 		this.steps = steps;
+		this.n_samples = n_samples;
 		this.ucPreset = ucPreset;
 		this.uc = uc;
 	}
 
-	public async generateImage() {
+	public async generateImage(): Promise<string[]> {
 		const response = await axios({
 			method: "post",
 			baseURL: this.url,
@@ -60,16 +63,37 @@ class novelAI {
             },
 			data: {
                 "input": this.input,
-                "number": this.numberOfPics,
-                "model": this.model,
-                "resolution": this.resolution,
-                "scale": this.scale,
-                "sampler": this.sampler,
-                "steps": this.steps,
-                "ucPreset": this.ucPreset,
-                "uc": this.uc
+				"model": this.model,
+				"parameters": {
+					"resolution": this.resolution,
+					"scale": this.scale,
+					"sampler": this.sampler,
+					"steps": this.steps,
+					"n_samples": this.n_samples,
+					"ucPreset": this.ucPreset,
+					"uc": this.uc
+				}
             }
-		})
+		});
+
+		if(response.status == 201) {
+			const data = response.data as string;
+			let image = data.replace("event: newImage\nid: 1\ndata:", "");
+			let imageArr = [];
+			for(let i=1; i<=this.n_samples; i++) {
+				const imageSplit = image.split(`\nevent: newImage\nid: ${i+1}\ndata:`);
+				const buffer = Buffer.from(imageSplit[0], "base64");
+				const filename = path.join(process.cwd(), "prod", `${Date.now()}-${i}.png`);
+				imageArr.push(filename);
+				fs.writeFileSync(filename, buffer);
+				image = imageSplit.length == 2 ? imageSplit[1] : "";
+			}
+			return imageArr;
+		}
+		else {
+			console.log("Error");
+			return [];
+		}
 	}
 }
 
